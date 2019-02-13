80a65b5d3d5ae2aa03f3826ed33fface7b75a23b
roy-tian <tdjol@hotmail.com>
2019-02-13 16:09:28 +0800

    update main.js, upload zip

--- html/beginner-site-scripted/scripts/main.js
+++ html/beginner-site-scripted/scripts/main.js
@@ -1,30 +1,33 @@
-// 图片切换器
-const myImage = document.querySelector('img');
+// 图片切换代码
+let myImage = document.querySelector('img');
 
 myImage.onclick = function() {
-  const mySrc = myImage.getAttribute('src');
+  let mySrc = myImage.getAttribute('src');
   if(mySrc === 'images/firefox-icon.png') {
-    myImage.setAttribute ('src','images/firefox2.png');
+    myImage.setAttribute('src','images/firefox2.png');
   } else {
-    myImage.setAttribute ('src','images/firefox-icon.png');
+    myImage.setAttribute('src','images/firefox-icon.png');
   }
-}
+};
 
-// 自定义欢迎信息
-const myButton = document.querySelector('button');
-const myHeading = document.querySelector('h1');
-
-myButton.onclick = setUserName;
-
-if(!localStorage.getItem('name')) {
-  setUserName();
-} else {
-  const storedName = localStorage.getItem('name');
-  myHeading.innerHTML = 'Mozilla 酷毙了，' + storedName  + '！';
+// 个性化欢迎信息
+function setHeading(name) {
+  let myHeading = document.querySelector('h1');
+  myHeading.textContent = 'Mozilla 酷毙了，' + name + '！';
 }
 
 function setUserName() {
-  const myName = prompt('请输入你的名字');
+  let myName = prompt('请输入你的名字');
   localStorage.setItem('name', myName);
-  myHeading.innerHTML = 'Mozilla 酷毙了，' + myName + '！';
+  setHeading(myName);
+} 
+
+let storedName = localStorage.getItem('name');
+if(!storedName) {
+   setUserName();
+} else {
+   setHeading(storedName);
 }
+
+let myButton = document.querySelector('button'); 
+myButton.onclick = setUserName;
