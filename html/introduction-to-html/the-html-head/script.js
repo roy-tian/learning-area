b1141c963a8c752af23edbfad47732193eeb7a92
roy-tian <tdjol@hotmail.com>
2020-05-28 00:27:45 +0800

    translate

--- html/introduction-to-html/the-html-head/script.js
+++ html/introduction-to-html/the-html-head/script.js
@@ -1,21 +1,21 @@
-var list = document.createElement('ul');
-var info = document.createElement('p');
-var html = document.querySelector('html');
+const list = document.createElement('ul');
+const info = document.createElement('p');
+const html = document.querySelector('html');
 
-info.textContent = 'Below is a dynamic list. Click anywhere outside the list to add a new list item. Click an existing list item to change its text to something else.';
+info.textContent = '以下是一个动态列表。点击列表外任意位置可添加新的列表项。点击已有的列表项可修改内容。';
 
 document.body.appendChild(info);
 document.body.appendChild(list);
 
 html.onclick = function() {
-  var listItem = document.createElement('li');
-  var listContent = prompt('What content do you want the list item to have?');
+  const listItem = document.createElement('li');
+  const listContent = prompt('希望列表中显示什么内容？');
   listItem.textContent = listContent;
   list.appendChild(listItem);
 
   listItem.onclick = function(e) {
     e.stopPropagation();
-    var listContent = prompt('Enter new content for your list item');
+    const listContent = prompt('为列表项输入新的内容');
     this.textContent = listContent;
-  }
-}
+  };
+};
